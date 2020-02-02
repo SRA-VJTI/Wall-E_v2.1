600f2b33c68c0252fb870a229f9b482b1bd37f69
laukik-hase <laukik17@gmail.com>
2020-02-02 16:50:06 +0530

    Modified Script

--- wall_e_install.sh
+++ wall_e_install.sh
@@ -1,22 +1,28 @@
-#Prerequistes
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git wget libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
-cd ~
-mkdir esp && cd esp
+# Check if IDF Exists
+temp_path="${HOME}/esp/esp-idf"
 
-#ESP Toolchain & ESP-IDF
-git clone --recursive https://github.com/espressif/esp-idf.git
-cd esp-idf
-./install.sh
-. $HOME/esp/esp-idf/export.sh
-echo ". $HOME/esp/esp-idf/export.sh" >> ~/.profile
-source ~/.profile
+if [ "$(realpath "$IDF_PATH")" = "$(realpath "$temp_path")" ]; then
+    echo "ESP IDF already exists"
+else
+    echo "Installing ESP IDF"
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get install git wget libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
+    cd ~
+    mkdir esp && cd esp
 
-#Wall-E files and custom configurations
+    #ESP Toolchain & ESP-IDF
+    git clone --recursive https://github.com/espressif/esp-idf.git
+    cd esp-idf
+    ./install.sh
+    . $HOME/esp/esp-idf/export.sh
+    echo ". $HOME/esp/esp-idf/export.sh" >> ~/.profile
+    source ~/.profile
+fi
+
+# Wall-E files and custom configurations
 cd ~
 git clone https://github.com/SRA-VJTI/Wall-E_v2.1
-cd Wall-E_v2.1
 
 
 
